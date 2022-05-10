820e9611b68568e4d28c791612018e12422a7086
pwarias <93276356+PiotrWarias@users.noreply.github.com>
2022-05-10 11:13:36 +0000

    5_2

--- script.js
+++ script.js
@@ -32,7 +32,20 @@
   
 
   cw2.addEventListener("click", function () {
-    //TODO implement it
+    answer.innerHTML = "Loading...";
+    fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(response => response.json())
+        .then(function (data){
+          appendData(data);
+        })
+    function appendData(data){
+      answer.innerHTML = "";
+      for (var i = 0; i <data.length; i++){
+        var div = document.createElement("div");
+        div.innerHTML= 'UserID:' + data[i].userId + '<br> id:' + data[i].id + '  <br>title: ' + data[i].title + '<br> body: '+ data[i].body + '<br><br>';
+        answer.appendChild(div);
+      }
+    }
   })
 
   cw3.addEventListener("click", function () {
