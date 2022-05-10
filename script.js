9535277225e389815a8763eb5d67da76a99801e3
pwarias <93276356+PiotrWarias@users.noreply.github.com>
2022-05-10 11:11:33 +0000

    5_1

--- script.js
+++ script.js
@@ -15,8 +15,21 @@
   })
 
   cw1.addEventListener("click", function () {
-    //TODO implement it
+    fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(response => response.json())
+        .then(function (data){
+          appendData(data);
+        })
+    function appendData(data){
+      answer.innerHTML = "";
+      for (var i = 0; i <data.length; i++){
+        var div = document.createElement("div");
+        div.innerHTML= 'UserID:' + data[i].userId + '<br> id:' + data[i].id + '  <br>title: ' + data[i].title + '<br> body: '+ data[i].body + '<br><br>';
+        answer.appendChild(div);
+      }
+    }
   })
+  
 
   cw2.addEventListener("click", function () {
     //TODO implement it
